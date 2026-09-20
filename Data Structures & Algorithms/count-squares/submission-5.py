9c15241f4b03b05eb533be60999e530ea8bf0123
Hao-Wei Lee <hwlee.to@gmail.com>
2026-09-20 14:53:29 -0400

    Add: count-squares - submission-5

---
+++ Data Structures & Algorithms/count-squares/submission-5.py
@@ -0,0 +1,25 @@
+class CountSquares:
+
+    def __init__(self):
+        self.points = {}
+
+    def add(self, point: List[int]) -> None:
+        key = tuple(point)
+        self.points[key] = self.points.get(key, 0) + 1
+        
+
+    def count(self, point: List[int]) -> int:
+        count = 0
+        for key in self.points.keys():
+            x2 = (point[0]-key[0])**2
+            if x2 == 0: continue 
+            y2 = (point[1]-key[1])**2
+            if y2 == 0:continue
+            
+            if x2 == y2 :
+                k1 = (key[0], point[1])
+                k2 = (point[0], key[1])
+                count += self.points[key]*self.points.get(k1, 0)*self.points.get(k2, 0)
+        return count
+
+        
